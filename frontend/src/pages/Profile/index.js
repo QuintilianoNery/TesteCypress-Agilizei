c2352ff6bd56dfbb5a5c2b9efb0a97fc8bc7f2b3
Quintiliano Paquini Nery <quintiliano@tecsystem.com.br>
2020-12-02 22:32:45 -0300

    Aula 02 - data-cy deletar caso

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -70,7 +70,7 @@ export default function Profile() {
             <strong>VALOR:</strong>
             <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-            <button onClick={() => handleDeleteIncident(incident.id)} type="button">
+            <button data-cy="button-deletar-caso" onClick={() => handleDeleteIncident(incident.id)} type="button">
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
